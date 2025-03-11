d08302b2054795ad3fa1fc4a8a0a04cc5665f0f9
Chen Wang <wang.756090@gmail.com>
2025-03-11 12:11:44 +0000

    Feat: Add more stats for single station route

--- src/stations/stations.service.ts
+++ src/stations/stations.service.ts
@@ -14,7 +14,64 @@ export class StationService {
     return await this.stationRepository.find({ skip: skip * take, take });
   }
 
-  async getSingleStation(id: number): Promise<Station | null> {
-    return await this.stationRepository.findOne({ where: { id } });
+  async getSingleStation(id: number): Promise<{
+    station_name: string;
+    station_address: string;
+    start_count: number;
+    return_count: string;
+    start_average: number;
+    return_average: number;
+  } | null> {
+    const station: {
+      station_name: string;
+      station_address: string;
+      start_count: number;
+      return_count: string;
+      start_average: number;
+      return_average: number;
+    } | null = await this.stationRepository.manager.query(
+      `
+        SELECT
+          s.station_name AS station_name,
+          s.station_address AS station_address,
+          (
+            SELECT
+              COUNT(id)
+            FROM
+              journey
+            WHERE
+              departure_station_id = ${id}
+          ) AS start_count,
+          (
+            SELECT
+              COUNT(id)
+            FROM
+              journey
+            WHERE
+              return_station_id = ${id}
+          ) AS return_count,
+          (
+            SELECT
+              AVG(distance)
+            FROM
+              journey
+            WHERE
+              departure_station_id = ${id}
+          ) AS start_average,
+          (
+            SELECT
+              AVG(distance)
+            FROM
+              journey
+            WHERE
+              return_station_id = ${id}
+          ) AS return_average
+        FROM
+          station s
+        WHERE
+          s.id = ${id};
+      `,
+    );
+    return station;
   }
 }

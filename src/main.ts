2a22292591b4da92caefab89c658252cb5b5422a
Chen Wang <wang.756090@gmail.com>
2025-05-15 15:16:02 +0000

    Feat: Add Swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,15 @@ async function bootstrap() {
     credentials: true,
   });
   app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle('Journey analyser API')
+    .setDescription('API for Journey analyser')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
